3a504848adf5c794fcb594e3771e8403221df032
andrewguo5 <Andrew G>
2014-05-24 21:07:21 -0400

    added accessor and mutator methods for grid and gem classes. added method to check for combos

--- Gem.java
+++ Gem.java
@@ -2,5 +2,69 @@
 public class Gem
 {
     private int type;
+    private int xcor;
+    private int ycor;
+    
+    public Gem () {
+	type = 0;
+    }
 
+    public Gem (int val) {
+	type = val;
+    }
+    
+    public int getType() {
+	return type;
+    }
+
+    public int getXcor() {return xcor;}
+    public int getYcor() {return ycor;}
+
+    public Gem getGem(Grid gemGrid, int dx, int dy) {
+	return gemGrid.getGem(xcor + dx, ycor + dy);
+    }
+
+    public Gem getLeft(Grid gemGrid) {
+	return gemGrid.getGem(xcor - 1, ycor);
+    }
+    public Gem getRight(Grid gemGrid) {
+	return gemGrid.getGem(xcor + 1, ycor);
+    }
+    public Gem getUp(Grid gemGrid) {
+	return gemGrid.getGem(xcor, ycor - 1);
+    }
+    public Gem getDown(Grid gemGrid) {
+	return gemGrid.getGem(xcor, ycor + 1);
+    }
+    
+
+    public void removeSelf (Grid gemGrid) {
+	gemGrid.setNull(xcor, ycor);
+    }
+
+    public boolean equals(Gem obj) {
+	return type == obj.getType();
+    }
+
+    public boolean isCombo(Grid g) {
+	if (this.equals(getLeft(g) ) ) {
+	    if (getLeft(g).equals(getLeft(g).getLeft(g))) {
+		return true;
+	    }
+	    else if (this.equals(getRight(g) ) ) {
+		return true;
+	    }
+	}
+	else if ( (this.equals(getUp(g) ) ) ){
+	    if (getUp(g).equals(getUp(g).getUp(g) ) ) {
+		return true;
+	    }
+	    else if (this.equals(getDown(g) ) ) {
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    
 }
--- Grid.java
+++ Grid.java
@@ -8,6 +8,7 @@ public class Grid
     private int tileSize = 40; // size of each slot that holds a gem
     
     private Tile[][] grid;
+    private Gem[][] gemGrid;
     
 
     public Grid(){
@@ -17,6 +18,21 @@ public class Grid
 		grid[y][x] = new Tile((x*tileSize),(y*tileSize),tileSize,tileSize); //this places the Tiles in correct location on GUI
 	    }
 	}
+    }
+
+    public void setNull(int x, int y) 
+    {
+	gemGrid[x][y] = null;
+    }
+
+    public Gem getGem (int x, int y)
+    {
+	return gemGrid[x][y];
+    }
+
+    public Tile getTile (int x, int y)
+    {
+	return grid[x][y];
     }
 
 
